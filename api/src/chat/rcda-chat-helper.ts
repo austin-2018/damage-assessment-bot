c46847e1514815382d0628c4f79bdeaf5478924d
Jituri, Pranav <Pranav.Jituri@appliedis.com>
2018-10-04 17:07:28 +0530

    Created a Chat Helper to store entities useful during a chat

---
+++ api/src/chat/rcda-chat-helper.ts
@@ -0,0 +1,16 @@
+export const sectorList:Array<string> = [
+    "Health",
+    "Food",
+    "Wash",
+    "Shelter/NFI",
+    "Protection",
+    "Education",
+    "Livelihood",
+    "Other"
+]
+
+export const geographicalSetting:Array<string> = [
+    "Urban",
+    "Rural",
+    "Semi-Urban"
+]
